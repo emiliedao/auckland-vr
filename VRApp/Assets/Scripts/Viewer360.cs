2b72d5c6b1db9b55cf7443df99cd449865f040d7
Emilie <emilie.dao@enseirb-matmeca.fr>
2018-06-20 11:23:41 +1200

    improved

---
+++ VRApp/Assets/Scripts/Viewer360.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Viewer360 : MonoBehaviour {
+
+    public void Start()
+    {
+        
+    }
+
+    public void PickImageFromGallery()
+    {
+        if (NativeGallery.IsMediaPickerBusy())
+        {
+            Debug.Log("busy");
+        }
+    }
+
+}
